1e3f6bae7ff5c55da4e0323dfde1780fcde81ee5
mindyhwangmeihui@gmail.com <mindyhwangmeihui@gmail.com>
2020-11-05 01:30:40 +0800

    Added Hospital Input File Added Import Hospital File Updated Import Graph File

--- src/ProgramGUI.java
+++ src/ProgramGUI.java
@@ -9,45 +9,43 @@ import java.util.regex.Matcher;
 public class ProgramGUI {
 
     /*Set defaultDirectory
-    * Change the path for defaultDirectory
-    * Windows path: public static String defaultDirectory = "C:\\Users\\mindy\\IdeaProjects\\ALGO_GIT\\";
-    * Mac path: public static String defaultDirectory = "/Users/wenjun/Downloads/14_9/"; */
+     * Change the path for defaultDirectory
+     * Windows path: public static String defaultDirectory = "C:\\Users\\mindy\\IdeaProjects\\ALGO_GIT\\";
+     * Mac path: public static String defaultDirectory = "/Users/wenjun/Downloads/14_9/"; */
 
     //Windows path
     public static String defaultDirectory = "C:\\Users\\mindy\\IdeaProjects\\ALGO_GIT\\src\\input\\";
-    public static File file;
+    public static File graphFile, hospitalFile;
 
     public static void main(String[] args) {
         //GUI Window
         JFrame frame = new JFrame("PT2 GrpB2 Graph Algorithm Program");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1250,550);
+        frame.setSize(1250, 550);
 
         //Graph Label
         JLabel graphLabel;
         graphLabel = new JLabel("Graph File: ");
-        graphLabel.setBounds(50,50,100,30);
+        graphLabel.setBounds(50, 50, 100, 30);
 
         //Display Graph File Name (TextBox)
         JTextField graphFileName;
         graphFileName = new JTextField();
-        graphFileName.setBounds(225,50,300,30);
+        graphFileName.setBounds(225, 50, 300, 30);
 
 
         //Graph File Button
         JButton graphFButton = new JButton("Import Graph File");
-        graphFButton.setBounds(550,50,150,30);
-        graphFButton.addActionListener(new ActionListener(){
+        graphFButton.setBounds(550, 50, 150, 30);
+        graphFButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-                try{
-                    file=chooseFileDialog();
-                }
-                catch(Exception ex){
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    graphFile = chooseFileDialog();
+                } catch (Exception ex) {
                     System.out.println(ex);
-                }
-                finally {
-                    ReadSourceFile(file);
+                } finally {
+                    readGraphFile(graphFile);
                 }
             }
         });
@@ -55,29 +53,41 @@ public class ProgramGUI {
         //Hospital Label
         JLabel hospitalLabel;
         hospitalLabel = new JLabel("Hospital File: ");
-        hospitalLabel.setBounds(50,100,100,30);
+        hospitalLabel.setBounds(50, 100, 100, 30);
 
         //Display Hospital File Name (TextBox)
         JTextField hospitalFileName;
         hospitalFileName = new JTextField();
-        hospitalFileName.setBounds(225,100,300,30);
+        hospitalFileName.setBounds(225, 100, 300, 30);
 
         //Hospital File Button
         JButton hospitalFButton = new JButton("Import Hospital File");
-        hospitalFButton.setBounds(550,100,150,30);
+        hospitalFButton.setBounds(550, 100, 150, 30);
+        hospitalFButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    hospitalFile = chooseFileDialog();
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                } finally {
+                    readHospitalFile(hospitalFile);
+                }
+            }
+        });
 
         //Algorithm Label
         JLabel algoLabel;
         algoLabel = new JLabel("Algorithm: ");
-        algoLabel.setBounds(50,150,100,30);
+        algoLabel.setBounds(50, 150, 100, 30);
 
         //Algorithm Radio Button 1
         JRadioButton algo1 = new JRadioButton("1: Algorithm 1");
-        algo1.setBounds(225,150,150,30);
+        algo1.setBounds(225, 150, 150, 30);
 
         //Algorithm Radio Button 2
         JRadioButton algo2 = new JRadioButton("2: Algorithm 2");
-        algo2.setBounds(225,180,150,30);
+        algo2.setBounds(225, 180, 150, 30);
 
         //Algorithm Button Group
         ButtonGroup bg = new ButtonGroup();
@@ -87,55 +97,55 @@ public class ProgramGUI {
         //Input Node Size Label
         JLabel nSizeLabel;
         nSizeLabel = new JLabel("Input # of Nodes: ");
-        nSizeLabel.setBounds(50,250,150,30);
+        nSizeLabel.setBounds(50, 250, 150, 30);
 
         //Input Node Size TextBox
         JTextField nSizeInput;
         nSizeInput = new JTextField();
-        nSizeInput.setBounds(225,250,150,30);
+        nSizeInput.setBounds(225, 250, 150, 30);
 
         //Input Hospital Size Label
         JLabel hSizeLabel;
         hSizeLabel = new JLabel("Input # of Hospitals: ");
-        hSizeLabel.setBounds(50,300,150,30);
+        hSizeLabel.setBounds(50, 300, 150, 30);
 
         //Input Hospital Size TextBox
         JTextField hSizeInput;
         hSizeInput = new JTextField();
-        hSizeInput.setBounds(225,300,150,30);
+        hSizeInput.setBounds(225, 300, 150, 30);
 
         //Input Top K Hospital Size Label
         JLabel kSizeLabel;
         kSizeLabel = new JLabel("Input # of Top-K Hospitals: ");
-        kSizeLabel.setBounds(50,350,200,30);
+        kSizeLabel.setBounds(50, 350, 200, 30);
 
         //Input Top K Hospital Size TextBox
         JTextField kSizeInput;
         kSizeInput = new JTextField();
-        kSizeInput.setBounds(225,350,150,30);
+        kSizeInput.setBounds(225, 350, 150, 30);
 
         //Output File Label
         JLabel outputLabel;
         outputLabel = new JLabel("Output File Name: ");
-        outputLabel.setBounds(50,400,200,30);
+        outputLabel.setBounds(50, 400, 200, 30);
 
         //Output File Name (TextBox)
         JTextField outputFile;
         outputFile = new JTextField();
-        outputFile.setBounds(225,400,300,30);
+        outputFile.setBounds(225, 400, 300, 30);
 
         //Generate Algorithm Button
         JButton transverseButton = new JButton("Transverse");
-        transverseButton.setBounds(225,450,150,30);
+        transverseButton.setBounds(225, 450, 150, 30);
 
         //Program Run Label
         JLabel programLabel;
         programLabel = new JLabel("Program Run: ");
-        programLabel.setBounds(725,15,100,30);
+        programLabel.setBounds(725, 15, 100, 30);
 
         //Program Run Display (TextArea + ScrollPane)
         JTextArea programTextArea = new JTextArea();
-        programTextArea.setBounds(725,45,475,450);
+        programTextArea.setBounds(725, 45, 475, 450);
 
 
         //GUI Frame Components
@@ -172,18 +182,16 @@ public class ProgramGUI {
         int returnVal = fc.showOpenDialog(null);
 
         //User selected file
-        if(returnVal==JFileChooser.APPROVE_OPTION)
-        {
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             //Retrieve selected file
             return fc.getSelectedFile();
         }
         return null;
     }
 
-  public static HashMap<Integer, ArrayList<Integer>> ReadSourceFile(File file)
-    {
+    public static HashMap<Integer, ArrayList<Integer>> readGraphFile(File file) {
         String input = null;
-        int counter=0;
+        int counter = 0;
 
         //Hashmap to store ArrayList
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<Integer, ArrayList<Integer>>();
@@ -195,21 +203,20 @@ public class ProgramGUI {
 
             System.out.println("File reading...");
             while ((input = br.readLine()) != null) {
-                if((input.charAt(0)-'#')!=0)
-                {
+                if ((input.charAt(0) - '#') != 0) {
                     String[] stringNode = input.split("\t");
                     int startNode = Integer.parseInt(stringNode[0]);
                     int endNode = Integer.parseInt(stringNode[1]);
 
                     nodeList = graph.get(startNode);
-                    if(nodeList == null){
+                    if (nodeList == null) {
                         nodeList = new ArrayList<Integer>();
                         nodeList.add(endNode);
-                    }else{
-                        if(!nodeList.contains(endNode))
+                    } else {
+                        if (!nodeList.contains(endNode))
                             nodeList.add(endNode);
                     }
-                   //System.out.println(startNode +" "+ endNode);
+                    //System.out.println(startNode +" "+ endNode);
                     graph.put(startNode, nodeList);
                 }
 
@@ -221,6 +228,39 @@ public class ProgramGUI {
 
             //System.out.println(graph);
             return graph;
+
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error processing the file!");
+            return null;
+        }
+
+    }
+    public static HashMap<Integer, Integer> readHospitalFile(File file)
+    {
+        String input = null;
+        int counter=0;
+
+        //Hashmap to store ArrayList
+        HashMap<Integer, Integer> hospital = new HashMap<Integer, Integer>();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            //Start time calculation
+            long start = System.currentTimeMillis();
+
+            System.out.println("File reading...");
+            while ((input = br.readLine()) != null) {
+                if((input.charAt(0)-'#')!=0)
+                {
+                    hospital.put(Integer.parseInt(input),1);
+                }
+            }
+
+            //Stop time calculation
+            long stop = System.currentTimeMillis();
+            System.out.println("File Reading Time (ms): " + (stop - start));
+
+            System.out.println(hospital);
+            return hospital;
 
         } catch (IOException | NumberFormatException e) {
             System.out.println("Error processing the file!");
